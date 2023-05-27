cc85cda6451d5233492bfe822635fb68e6b7a377
Rutvij Sathe <rut26sathe@gmail.com>
2023-05-27 11:11:37 -0400

    createUser, login User, Changepassword complete

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,14 @@
-import { asNexusMethod, intArg, makeSchema, objectType } from 'nexus'
+import {
+  asNexusMethod,
+  intArg,
+  makeSchema,
+  nonNull,
+  objectType,
+  stringArg,
+} from 'nexus'
 import { Context } from './context'
 import { DateTimeResolver } from 'graphql-scalars'
+import { compare, hash } from 'bcryptjs'
 
 const DateTime = asNexusMethod(DateTimeResolver, 'date')
 
@@ -49,7 +57,7 @@ const Movie = objectType({
 const Query = objectType({
   name: 'Query',
   definition(t) {
-    // Query All Users
+    /** Query All Users */
     t.nonNull.list.nonNull.field('allUsers', {
       type: User,
       resolve: (_parent, _args, context: Context) => {
@@ -57,7 +65,9 @@ const Query = objectType({
       },
     })
 
-    // Query All Movies -> should include sorting, filtering and pagination
+    /** Query All Movies
+     *  Constraints: should include sorting, filtering, pagination
+     *  */
     t.nonNull.list.field('allMovies', {
       type: Movie,
       resolve: (_parent, _args, context: Context) => {
@@ -65,7 +75,9 @@ const Query = objectType({
       },
     })
 
-    // Query Movie by id
+    /** Query Movie by id
+     *  Required arguments: id
+     */
     t.nullable.field('movieById', {
       type: Movie,
       args: {
@@ -77,11 +89,154 @@ const Query = objectType({
         })
       },
     })
+
+    /** Search Movies By Name or Description
+     *  */
+  },
+})
+
+const Mutation = objectType({
+  name: 'Mutation',
+  definition(t) {
+    /** TODO: Sign Up User
+     *  Constraints: Password should be hashed before being sent to the server, JWT should be sent after registration
+     * */
+    t.nonNull.field('createUser', {
+      type: 'User',
+      args: {
+        username: nonNull(stringArg()),
+        email: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+      },
+      resolve: async (_parent, args, context: Context) => {
+        try {
+          const userCheck = await context.prisma.user.findFirst({
+            where: {
+              email: args.email!,
+            },
+          })
+
+          if (userCheck !== null) {
+            console.log(`User with email ${userCheck} already exists`)
+            throw new Error('User already exists!')
+          }
+
+          const hashedPassword = await hash(args.password, 12)
+
+          return context.prisma.user.create({
+            data: {
+              username: args.username,
+              email: args.email,
+              password: hashedPassword,
+            },
+          })
+        } catch (error) {
+          console.log(error)
+          throw new Error(`${error}`)
+        }
+      },
+    })
+
+    /** TODO: Sign In User
+     *  Constraints:  Password should be hashed before being sent to the server
+     *                JWT Token should be provided upon login
+     * */
+
+    t.field('loginUser', {
+      type: 'String',
+      args: {
+        email: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+      },
+      resolve: async (_parent, args, context: Context) => {
+        try {
+          const user = await context.prisma.user.findFirst({
+            where: {
+              email: args.email,
+            },
+          })
+
+          if (!user) {
+            throw new Error(`User not found!`)
+          }
+
+          const validatePasword = await compare(args.password, user.password)
+
+          if (!validatePasword) {
+            throw new Error(`Invalid password!`)
+          }
+
+          return 'Login Successful!'
+        } catch (error) {
+          console.log(error)
+          throw new Error(`${error}`)
+        }
+      },
+    })
+
+    /** TODO: Change Password
+     *  Requried Arguments: email, currentPassword, newPassword
+     *  */
+
+    t.field('changePassword', {
+      type: 'String',
+      args: {
+        email: nonNull(stringArg()),
+        currentPassword: nonNull(stringArg()),
+        newPassword: nonNull(stringArg()),
+      },
+      resolve: async (_parent, args, context: Context) => {
+        try {
+          const user = await context.prisma.user.findFirst({
+            where: {
+              email: args.email,
+            },
+          })
+          if (!user) {
+            throw new Error(`User not found!`)
+          }
+          // checking if the password is the same
+          const validatePasword = await compare(
+            args.currentPassword,
+            user.password,
+          )
+          if (!validatePasword) {
+            throw new Error(`Invalid Current Password!`)
+          }
+          const hashedPassword = await hash(args.newPassword, 12)
+          await context.prisma.user.update({
+            where: {
+              id: user.id,
+            },
+            data: {
+              password: hashedPassword,
+            },
+          })
+          return 'Password Changed!'
+        } catch (error) {
+          console.log(error)
+          throw new Error(`${error}`)
+        }
+      },
+    })
+
+    /** TODO: Create a Movie
+     *  Requests should be authenticated using a JWT token in header
+     *  Arguments:
+     *  */
+    /** TODO: Update a Movie
+     *  Requests should be authenticated using a JWT token in header
+     *  Arguments:
+     *  */
+    /** TODO: Delete a Movie
+     *  Requests should be authenticated using a JWT token in header
+     *  Arguments:
+     *  */
   },
 })
 
 export const schema = makeSchema({
-  types: [User, Movie, Query, DateTime],
+  types: [User, Movie, Query, Mutation, DateTime],
   outputs: {
     schema: __dirname + '/schema.graphql',
     typegen: __dirname + '/generated/nexus.ts',
